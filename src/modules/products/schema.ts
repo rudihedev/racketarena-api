44c88688b5dc27d4e344a8d48c4dc92cdb0e61ee
rudihedev <rudihedev@gmail.com>
2026-03-08 14:04:57 +0700

    feat: update schema

--- src/modules/products/schema.ts
+++ src/modules/products/schema.ts
@@ -5,12 +5,23 @@ export const ProductSchema = z.object({
   brand: z.string().min(1),
   name: z.string().min(1),
   slug: z.string().min(1),
-  racketWeightU: z.enum(["2U", "3U", "4U", "5U"]),
+  racketWeightU: z.enum(["2U", "3U", "4U", "5U", "6U"]),
   createdAt: z.date(),
   updatedAt: z.date(),
 });
+
+export const SeedProductSchema = ProductSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export const SeedProductsSchema = SeedProductSchema.array();
 
 export const ProductsSchema = z.array(ProductSchema);
 
 export type Product = z.infer<typeof ProductSchema>;
 export type Products = z.infer<typeof ProductsSchema>;
+
+export type SeedProduct = z.infer<typeof SeedProductSchema>;
+export type SeedProducts = z.infer<typeof SeedProductsSchema>;
